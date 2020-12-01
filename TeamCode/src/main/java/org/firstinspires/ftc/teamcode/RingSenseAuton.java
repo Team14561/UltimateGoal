8d1a8c87b74244fbd78ddb67710cec476d0d4954
Touiquette <JanisKF@gmail.com>
2020-11-30 18:51:01 -0600

    wWrote the ring sense

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RingSenseAuton.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RingSenseAuton.java
@@ -12,6 +12,13 @@ public class RingSenseAuton {
     private ElapsedTime runtime;
     private HeightSensor heightSensor;
     private Arm arm;
+    private double armEncoderStart;
+    private double armEncoderEnd = 1317.0;
+    private double oldHeight;
+    private double newHeight;
+    public int ringNumber;
+    private double sensorWait;
+
 
 
     public RingSenseAuton (HardwareMap hardwareMap,
@@ -21,17 +28,54 @@ public class RingSenseAuton {
         arm = new Arm(hardwareMap, telemetry);
     }
 
-    public void mainStages(){
+    public int mainStages(){
         arm.autonMaintain();
 
-        /*
-        if (stage == 0){
-            if (){
 
-            }
+        if (stage == 0){
+            armEncoderStart = arm.getEncoder;
+            arm.setPower(0.2);
+            expirationTime = runtime.time() + 5.0;
             stage = 1;
         }
-        */
+        //get values to look for jump
+        else if (stage == 1){
+            oldHeight = HeightSensor.getHeight;
+            sensorWait = runtime.time() + 0.1;
+            stage = 2:
+        }
+        else if (stage == 2) {
+            if (runtime.time() > sensorWait) {
+                newHeight = HeightSensor.getHeight;
+                stage = 3;
+            }
+        }
+        //if jump or time runs out stop arm and move on
+        else if (stage == 3) {
+            if ( oldHeight - newHeight > 50) || runtime.time() > expirationTime){
+                arm.setpower(0.0);
+                armEncoderEnd = arm.getEncoder;
+                stage = 4;
+            }
+            else {
+                stage = 1;
+            }
+        }
+        //figure out how far arm went and determine number of rings
+        else if (stage == 4) {
+            if (armEncoderStart - armEncoderEnd < 300) {
+                ringNumber = 3;
+            }
+            if (armEncoderStart - armEncoderEnd  > 300) {
+                ringNumber = 1;
+            }
+            //no end value set (timed out)
+            if (armEncoderEnd == 1317.0 ) {
+                ringNumber = 0;
+            }
+            return ringNumber;
+            stage = 5;
+        }
 
     }
 }
