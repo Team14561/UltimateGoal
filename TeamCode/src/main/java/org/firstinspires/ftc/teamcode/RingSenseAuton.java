a990e1f192393c53a4cd8efec444af1c988c4217
Touiquette <JanisKF@gmail.com>
2020-12-07 16:59:16 -0600

    wWrote the ring sense again but different

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RingSenseAuton.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RingSenseAuton.java
@@ -14,10 +14,8 @@ public class RingSenseAuton {
     private Arm arm;
     private double armEncoderStart;
     private double armEncoderEnd = 1317.0;
-    private double oldHeight;
-    private double newHeight;
+    private double height;
     public int ringNumber;
-    private double sensorWait;
 
     // Object variables mimicing gamepad control
     double armPower = 0.0;
@@ -37,7 +35,7 @@ public class RingSenseAuton {
             1. Move the arm down out of starting position
             2. Stop the arm when the encoder gets to scan distance
             3. Set the arm goal to position for 3-ring
-            4. Wait for arm to get into position
+            4. Wait for arm to get into position?
             5. Check distance sensor, if rings detected number equals three, goto stage 10
             6. Set the arm goal to position for 2-ring
             7. Wait for arm to get into position
@@ -53,41 +51,61 @@ public class RingSenseAuton {
             expirationTime = runtime.time() + 5.0;
             stage = 1;
         }
-        //get values to look for jump
-        else if (stage == 1) {
-            oldHeight = heightSensor.getHeight();
-            sensorWait = runtime.time() + 0.1;
-            stage = 2;
-        } else if (stage == 2) {
-            if (runtime.time() > sensorWait) {
-                newHeight = heightSensor.getHeight();
-                stage = 3;
-            }
+        if else (stage == 1) {
+           if (runtime.time > expirationTime) {
+               armPower = 0.0;
+               stage = 2;
+           }
         }
-        //if jump or time runs out stop arm and move on
-        else if (stage == 3) {
-            if (Math.abs(oldHeight - newHeight) > 50 || runtime.time() > expirationTime) {
+        if else (stage == 2) {
+            encoderGoal = encoderValue + 100;
+            //???
+            expirationTime = runtime.time() + 2.0;
+            stage = 3;
+        }
+        //???
+        if else (stage == 3) {
+            if (runtime.time > expirationTime) {
                 armPower = 0.0;
-                armEncoderEnd = arm.getEncoder();
                 stage = 4;
-            } else {
-                stage = 1;
             }
         }
-        //figure out how far arm went and determine number of rings
-        else if (stage == 4) {
-            if (armEncoderStart - armEncoderEnd < 300) {
+        if else (stage == 4) {
+            height = HeightSensor.getHeight();
+            if (Height < 100) {
                 ringNumber = 3;
-            } else if (armEncoderStart - armEncoderEnd > 300) {
+                stage = 8
+            }
+            else {
+                stage = 5;
+            }
+        }
+        if else (stage == 5) {
+            encoderGoal = encoderValue + 10;
+            //???
+            expirationTime = runtime.time() + 1.0;
+            stage = 6;
+        }
+        //???
+        if else (stage == 6) {
+            if (runtime.time > expirationTime) {
+                armPower = 0.0;
+                stage = 7;
+            }
+        }
+        if else (stage == 7) {
+            height = HeightSensor.getHeight();
+            if (Height < 100) {
                 ringNumber = 1;
-            } else {
-                ringNumber = 2;
+                stage = 8;
             }
-            //no end value set (timed out)
-            if (armEncoderEnd == 1317.0) {
+            else {
                 ringNumber = 0;
+                stage = 8;
             }
-            stage = 5;
+        }
+        if else (stage == 8) {
+
         }
     }
 }
